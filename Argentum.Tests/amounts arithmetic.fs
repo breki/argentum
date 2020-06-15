e531a57f0a0089ee94d8b2110861cb93acbf7a5c
Igor Brejc <igor.brejc@gmail.com>
2020-06-15 05:48:58 +0200

    Implement addAmount

--- Argentum.Tests/amounts arithmetic.fs
+++ Argentum.Tests/amounts arithmetic.fs
@@ -1,17 +1,54 @@
 ﻿module Argentum.Tests.``amounts arithmetic``
 
-open Argentum.ExchangeRates
 open Argentum.Model
-open Argentum.Tests.Builders
 open FsUnit
 open Xunit
 open Swensen.Unquote
 
+let addAmount (amountA: Amount) (amountB: Amount) =
+    match amountA.Divisor, amountB.Divisor with
+    | (divisorA, divisorB) when divisorA = divisorB ->
+        amount2 (amountA.Dividend + amountB.Dividend) divisorA
+    | (divisorA, divisorB) when divisorA % divisorB = 0 ->
+        let multiplier = divisorA / divisorB
+        amount2 (amountA.Dividend + amountB.Dividend * multiplier) divisorA
+    | (divisorA, divisorB) when divisorB % divisorA = 0 ->
+        let multiplier = divisorB / divisorA
+        amount2 (amountA.Dividend * multiplier + amountB.Dividend) divisorB
+    | (divisorA, divisorB) ->
+        let sumDivisor = divisorA * divisorB
+        amount2
+            (amountA.Dividend * divisorB + amountB.Dividend * divisorA)
+            sumDivisor
 [<Fact>]
 let ``amounts are same if dividends and divisors are the same``() =
     test <@ amount2 100 200 = amount2 100 200 @>
 
 [<Fact>]
+let ``amounts with different dividends are not the same``() =
+    test <@ amount2 100 200 <> amount2 150 200 @>
+
+[<Fact>]
+let ``amounts with different divisors are not the same``() =
+    test <@ amount2 100 200 <> amount2 100 250 @>
+
+[<Fact>]
 let ``amounts are same if ratios between dividend and divisor are the same``() =
     test <@ amount2 200 400 = amount2 100 200 @>
+    
+[<Fact>]
+let ``when adding amounts with the same divisor the sum retains the divisor``() =
+    test <@ amount2 100 100 |> addAmount (amount2 50 100) = amount2 150 100 @>
+
+[<Fact>]
+let ``when adding amounts with divisible divisors the sum uses the larger divisor (1)``() =
+    test <@ amount2 100 100 |> addAmount (amount2 50 200) = amount2 250 200 @>
+    
+[<Fact>]
+let ``when adding amounts with divisible divisors the sum uses the larger divisor (2)``() =
+    test <@ amount2 50 200 |> addAmount (amount2 100 100) = amount2 250 200 @>
+    
+[<Fact>]
+let ``when adding amounts with indivisible divisors the sum uses the multiplier divisor``() =
+    test <@ amount2 2 3 |> addAmount (amount2 2 5) = amount2 (2*5 + 2*3) 15 @>
     
